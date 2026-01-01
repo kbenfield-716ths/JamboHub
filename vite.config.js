334d8d1f818a3a09d04490d9ce0e50bd6f101478
Kyle Enfield <kbenfield@me.com>
2025-12-31 23:06:53 -0500

    fixed config file after ui fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/jamboree-hub/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/JamboHub/' : '/',
   server: {
     port: 3000
   }
